98af3b3ae22213274964444db63a1eec1da0075b
secki <seckinguzel@outlook.com>
2023-01-31 11:09:51 +0300

    Explanation has been added to the Solution class of package _14LongestCommonPrefix.

--- src/_14LongestCommonPrefix/Solution.java
+++ src/_14LongestCommonPrefix/Solution.java
@@ -18,8 +18,8 @@ class Solution {
         }
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) { //While indexOf the prefix string is not equal to zero. The indexOf() method returns the first index at which a specified element can be found in an array, or -1 if the element is not present.
-                prefix = prefix.substring(0, prefix.length() - 1); //While we cannot find the prefix at the begining of the array, for this example it is "flower" then it is going to decrease the size of the string. So it will be like that "flower"->"flowe"->"flow" then it is going to stop when until finish the loop.
+            while (strs[i].indexOf(prefix) != 0) { //While we cannot find the prefix for this example it is "flower".
+                prefix = prefix.substring(0, prefix.length() - 1); //It will decrease the size of the string. So it will be like that, first it will check for "flow" so process will be like that "flower"->"flowe"->"flow" in this step prefix is "flow". Then loop will continue, now prefix will be check for "flight" so the process will be like that "flow"->"flo"->"fl" and finish.
                 if (prefix.isEmpty()) {
                     return "";
                 }
