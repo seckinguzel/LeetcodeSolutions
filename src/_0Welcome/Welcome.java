aebeefb7e03843e70ec74e868ebb7f8c50cd4919
Seçkin Güzel <113905273+seckinguzel@users.noreply.github.com>
2024-02-03 14:22:26 +0300

    Welcome package has been added to the repository!

---
+++ src/_0Welcome/Welcome.java
@@ -0,0 +1,9 @@
+package _0Welcome;
+
+public class Welcome {
+//  Run this main method to test!
+    public static void main(String[] args) {
+        System.out.println("Welcome to Leetcode Solutions!\n" +
+                "By Seckin Guzel.");
+    }
+}
